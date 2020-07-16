a3abeb93637db2e0313c3184058d1b2b4488efca
Charles Ancheta <cancheta@ualberta.ca>
2020-07-15 21:30:37 -0600

    :poop: what am i doing with my life

--- server/type.js
+++ server/type.js
@@ -1,66 +1,74 @@
+// Play a certain level on typingclub.com
+// Usage:
+// node type.js [OPTIONS]
+// Options:
+// -h : HEADLESS, will not display the browser
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
-const string =
-  "energy equals mass * (multiplied by) the speed of light 2 (squared). The multiplication sign is usually left out, giving e is equal to mc 2 (squared).";
-const text = [
-  "E2 is equal to (m0c2)2 plus (pc)2",
-  "where",
-  "#1 E represents the energy of a particle",
-  "#2 m0 represents the mass of the particle when it is not moving",
-  "#3 p represents the momentum of the particle when it is moving",
-  "#4 c represents the speed of light.",
+const args = process.argv.slice(2);
+const headless = args[0] == "-h";
+const waitTime = 2;
+const symbols = [
+  "a!s@d#f$g%h^j&k*l(;) q!w@e3r$t%y^u&i*o(p) z!x@c#v$b%n^m&,*.(",
 ];
+const levelLink = "https://www.typingclub.com/sportal/program-3/396.play";
 
 const asyncForEach = async (array, callback) => {
   for (let i = 0; i < array.length; i++) await callback(array[i], i, array);
 };
 
-const main = async () => {
-  const browser = await puppeteer.launch({ slowMo: 14 });
+async function main() {
+  const browser = await puppeteer.launch({ headless, slowMo: 15 });
   const page = await browser.newPage();
-  await page.goto("https://www.typingclub.com/login.html", {
-    waitUntil: "networkidle0",
-  });
 
-  console.log("Logging in...");
-  await page.type("#username", process.env.EMAIL);
-  await page.type("#password", process.env.PASSWORD);
-  await page.click("#login-with-password");
+  async function login(email, password) {
+    console.log("Logging in...");
+    await page.goto("https://www.typingclub.com/login.html", {
+      waitUntil: "networkidle0",
+    });
+    await page.type("#username", email);
+    await page.type("#password", password);
+    await page.click("#login-with-password");
 
-  await page.waitForNavigation({ waitUntil: "networkidle0" });
-  await page.goto("https://www.typingclub.com/sportal/program-3/3270.play");
+    await page.waitForNavigation({ waitUntil: "networkidle0" });
+  }
+
+  const { EMAIL, PASSWORD } = process.env;
+  if (EMAIL != null) await login(EMAIL, PASSWORD);
+  else console.log("No EMAIL and PASSWORD defined in .env, playing as guest");
+
+  await page.goto(levelLink);
+
+  // remove annoying popup
   const classSelect =
     "#root > div:nth-child(1) > div > div > div > div > a:nth-child(3)";
   await page.waitForSelector(classSelect);
   await page.click(classSelect);
+
   let iterations = 0;
-  const navbar =
-    "#root > div:nth-child(2) > div.typing-results.scrollable-results > div.bottom-nav-bar > div";
-  const tryAgain =
-    "#root > div:nth-child(2) > div.typing-results.scrollable-results > div.bottom-nav-bar > div > button.btn.navbar-goback.hoverable-button.default > span";
 
   console.log("Starting rampage...");
   async function type(num) {
     await page.waitForSelector("body > input[type=text]");
-    await asyncForEach(text, async line => {
+    await asyncForEach(symbols, async line => {
       await page.keyboard.type(line);
       await page.keyboard.press("Enter");
     });
-    await page.waitFor(3000);
-    await page.goto("https://www.typingclub.com/sportal/program-3/3270.play");
+    await page.waitFor(waitTime * 1000);
+    await page.goto(levelLink);
     console.log("Iterations: " + iterations++);
     if (num > 0) {
       await type(num - 1);
     }
   }
+
   const arr = new Array(100).fill(0);
   await asyncForEach(arr, async () => {
     await asyncForEach(arr, async () => {
-      await type(100);
+      await type(100, symbols);
     });
   });
-};
+}
 
-if (process.env.EMAIL != null) main();
-else console.log("Must define email and password in .env file");
+main();
