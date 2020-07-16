f44cd80e68bbec980ddba2247c7058194722a610
Charles Ancheta <cancheta@ualberta.ca>
2020-07-16 11:37:46 -0600

    :sparkles: parses delivery progress properly

--- server/search.js
+++ server/search.js
@@ -1,44 +1,44 @@
-/**
- * @fileoverview Search developers.google.com/web for articles tagged
- * "Headless Chrome" and scrape results from the results page.
- */
 const puppeteer = require("puppeteer");
 
 const TRACKING_NUM = "1023038647106760";
+const args = process.argv.slice(2);
+const headless = args[0] == "headless";
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
+async function main() {
+  const browser = await puppeteer.launch({ headless });
   const page = await browser.newPage();
 
   // this whole thing is unnecessary tbh we could just go to the link to the progress page itself
   // https://www.canadapost.ca/trackweb/en#/details/1023038647106760
+  /*
   await page.goto("https://www.canadapost.ca/trackweb/en#/home");
   await page.focus("#input");
   await page.keyboard.type(TRACKING_NUM);
   await page.click("#submitButton");
   await page.waitForNavigation({ waitUntil: "networkidle0" });
+  */
 
-  // await page.goto("https://www.canadapost.ca/trackweb/en#/details/" + TRACKING_NUM);
+  await page.goto(
+    "https://www.canadapost.ca/trackweb/en#/details/" + TRACKING_NUM,
+    { waitUntil: "networkidle0" }
+  );
 
   await page.click("#showMoreLink");
-  // Wait for suggest overlay to appear and click "show all results".
-  // const allResultsSelector = ".devsite-suggest-all-results";
-  // await page.waitForSelector(allResultsSelector);
-  // await page.click(allResultsSelector);
-  // Wait for the results page to load and display the results.
-  // const resultsSelector = ".gsc-results .gsc-thumbnail-inside a.gs-title";
-  // await page.waitForSelector(resultsSelector);
-  // Extract the results from the page.
-  // const links = await page.evaluate(resultsSelector => {
-  //   const anchors = Array.from(document.querySelectorAll(resultsSelector));
-  //   return anchors.map(anchor => {
-  //     const title = anchor.textContent.split("|")[0].trim();
-  //     return `${title} - ${anchor.href}`;
-  //   });
-  // }, resultsSelector);
-  // console.log(links.join("\n"));
-  const deliveryProgress = await page.evaluate(selector => {
-    return document.getElementById(selector).outerHTML;
-  }, "trackprogresstable");
+
+  const deliveryProgress = await page.evaluate(() => {
+    const tableBody = document.querySelector("#trackprogresstable > tbody");
+    const tableRows = Array.from(tableBody.children);
+    const tableCells = tableRows.map(row => Array.from(row.children));
+    const cellContent = tableCells.map(row =>
+      row.map(cell => {
+        const child = Array.from(cell.children)[0];
+        return child != null ? child.innerHTML.trim() : "";
+      })
+    );
+    return cellContent;
+  });
+
   console.log(deliveryProgress);
-})();
+}
+
+main();
